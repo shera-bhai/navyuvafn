70b63aaab5dabe7f0d4c9c259e60b9d29c201642
Shera Bhai <harsh.yadav1746@gmail.com>
2024-07-01 18:52:34 +0530

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "//",
+  base: "/navyuvafn/",
   plugins: [react()],
   build: {
     outDir: 'build' // Change the output directory to 'build'
